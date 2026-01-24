08dc34f7d32a38adecbb36e740efe24916ba65d6
msam10 <msamantha1224@gmail.com>
2026-01-23 23:37:43 -0400

    mensajes de error

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,76 @@ function App() {
     imagen:"",
 });
 
+ const validateCard = (): boolean => {
+      let flag = true;
+      let nombreError = "";
+      let descripcionError = "";
+      let ataqueError = "";
+      let defensaError = "" ;
+      let tipoError = "";
+      let imagenError = "";
+
+      if(!card.nombre || card.nombre.length < 3 ) {
+        nombreError = "El nombre debe de tener al menos 3 caracteres";
+        flag = false;
+
+      }
+      if(!card.descripcion) {
+      descripcionError = "La descripcion es obligatoria";
+      flag = false;
+
+      }
+      if(!card.ataque || isNaN(card.ataque) || Number(card.ataque) < 0 ) {
+       ataqueError = "El ataque debe ser un numero positivo";
+      flag = false;
+
+      }
+      if(!card.defensa || isNaN(card.defensa) || Number(card.defensa) < 0 ) {
+       defensaError = "El defensa debe ser un numero positivo";
+      flag = false;
+
+      }
+      if(!card.tipo) {
+      tipoError = "El tipo es obligatorio";
+      flag = false;
+    
+      }
+      if(!card.imagen) {
+      imagenError = "La URL de la imagen es obligatoria";
+      flag = false;
+    
+      }
+
+       setErrors({
+        nombre:nombreError,
+        descripcion:descripcionError,
+        tipo:tipoError,
+        ataque:ataqueError,
+        defensa:defensaError,
+        imagen:imagenError,
+      
+      });
+
+      return flag;
+    };
+
+    const handleSubmit = () => {
+    
+     if(validateCard()){
+      alert("enviando carta...");
+     }
+
+
+    };
+
+    <button
+     className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded"
+     onClick={handleSubmit}
+    >
+      validar
+    </button>
+
+   
 
 
   return (
@@ -75,19 +145,27 @@ function App() {
      onChange={(e) => setCard({...card , imagen: e.target.value})}
       />
       
-      {errors.nombre && <p className="text-red-500"></p>}
-
       <button
       className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         validar
       </button>
 
+      {errors.nombre && <p className="text-red-500"></p>}
+      {errors.descripcion && <p className="text-red-500"></p>}
+      {errors.numero && <p className="text-red-500"></p>}
+      {errors.tipo && <p className="text-red-500"></p>}
+      {errors.defensa && <p className="text-red-500"></p>}
+      {errors.imagen && <p className="text-red-500"></p>}
+      
 
+      
 
 
     
     
      </div>
+     
+    
 
      ) }
 
