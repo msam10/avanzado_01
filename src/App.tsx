d4d14242c71aac2894f7eac16fa5d1970d118ddc
msam10 <msamantha1224@gmail.com>
2026-01-24 12:28:15 -0400

    formulario listooo

--- src/App.tsx
+++ src/App.tsx
@@ -21,10 +21,10 @@ function App() {
   const [errors,setErrors] = useState({
      nombre:"",
    descripcion:"",
-   numero:0,
+   numero:"",
     tipo:"",
-    ataque:0,
-    defensa:0,
+    ataque:"",
+    defensa:"",
     imagen:"",
 });
 
@@ -64,6 +64,7 @@ function App() {
       }
       if(!card.imagen) {
       imagenError = "La URL de la imagen es obligatoria";
+      window.alert(imagenError)
       flag = false;
     
       }
@@ -75,6 +76,7 @@ function App() {
         ataque:ataqueError,
         defensa:defensaError,
         imagen:imagenError,
+        numero:'',
       
       });
 
@@ -109,6 +111,7 @@ function App() {
      onChange={(e) => setCard({...card , nombre: e.target.value})}
      />
 
+     {errors.nombre && <p className="text-red-500">{errors.nombre}</p>}
 
      <input type="text"
      placeholder="descripcion"
@@ -116,6 +119,7 @@ function App() {
      value={card.descripcion}
      onChange={(e) => setCard({...card , descripcion: e.target.value})}
       />
+      {errors.descripcion && <p className="text-red-500">{errors.descripcion}</p>}
      
      <input type="number"
      placeholder="numero"
@@ -123,6 +127,7 @@ function App() {
      value={card.numero}
      onChange={(e) => setCard({...card , numero:Number( e.target.value)})}
       /> 
+      {errors.numero && <p className="text-red-500">{errors.numero}</p>}
 
       <input type="text"
      placeholder="tipo"
@@ -130,6 +135,7 @@ function App() {
      value={card.tipo}
      onChange={(e) => setCard({...card , tipo: e.target.value})}
       /> 
+      {errors.tipo && <p className="text-red-500">{errors.tipo}</p>}
 
       <input type="number"
      placeholder="defensa"
@@ -137,6 +143,16 @@ function App() {
      value={card.defensa}
      onChange={(e) => setCard({...card , defensa:Number( e.target.value)})}
       />  
+      {errors.defensa && <p className="text-red-500">¨{errors.defensa}</p>}
+
+
+      <input type="number"
+     placeholder="ataque"
+     className="border-2 border-indigo-500 outline-0 focus:border-indigo-700 rounder-nd p-2 w-full max-w-nd "
+     value={card.ataque}
+     onChange={(e) => setCard({...card , ataque:Number(e.target.value)})}
+      />
+      {errors.ataque && <p className="text-red-500">{errors.ataque}</p>}
       
       <input type="text"
      placeholder="imagen"
@@ -144,18 +160,21 @@ function App() {
      value={card.imagen}
      onChange={(e) => setCard({...card , imagen: e.target.value})}
       />
-      
+      {errors.imagen && <p className="text-red-500">{errors.imagen}</p>}
+
       <button
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      onClick={validateCard}
+      >
         validar
       </button>
 
-      {errors.nombre && <p className="text-red-500"></p>}
-      {errors.descripcion && <p className="text-red-500"></p>}
-      {errors.numero && <p className="text-red-500"></p>}
-      {errors.tipo && <p className="text-red-500"></p>}
-      {errors.defensa && <p className="text-red-500"></p>}
-      {errors.imagen && <p className="text-red-500"></p>}
+      
+      
+      
+     
+      
+      
       
 
       
